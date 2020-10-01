fd42a3c88ec2787c592fdbec508876d268b4fd94
Raul Montes <montesraul7@gmail.com>
2020-10-01 16:03:18 -0700

    Made the moving script (going over time) Also a few scene edits

---
+++ Tank Game/Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float tempSpeed;
+    public Transform ttf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //
+        ttf = this.gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ttf.position = transform.position + (transform.forward * tempSpeed * Time.deltaTime);
+    }
+}
