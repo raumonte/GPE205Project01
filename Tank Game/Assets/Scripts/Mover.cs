1d05474f0c26284d286aebfbc6b4fc0cd58020c2
Raul Montes <montesraul7@gmail.com>
2020-10-01 19:39:02 -0700

    Edited more of the Mover script

--- Tank Game/Assets/Scripts/Mover.cs
+++ Tank Game/Assets/Scripts/Mover.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    /// <summary>
+    /// Currently this script is being used to test out Time.deltaTime. Which takes things by secconds rather than
+    /// frames which is what the update does.
+    /// </summary>
     public float tempSpeed;
     public Transform ttf;
+    public float setTime;
+    public float countdown;
     // Start is called before the first frame update
     void Start()
     {
+        countdown = setTime;
         //
         ttf = this.gameObject.GetComponent<Transform>();
     }
@@ -16,6 +23,13 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0)
+        {
+            Debug.Log("reset");
+            countdown = setTime;
+        }
         ttf.position = transform.position + (transform.forward * tempSpeed * Time.deltaTime);
+       
     }
 }
